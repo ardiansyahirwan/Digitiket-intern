9ed95d768d1a437084aecd8c573132580d3b02e4
ardiansyahirwan <ardiansyahirwan923@gmail.com>
2020-10-23 13:02:05 +0700

    membuat alert

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
-   $('.gallery').slick({
-slidesToShow: 1,
-slidesToScroll: 1,
-arrows: false,
-fade: true,
-asNavFor: '.thumbnail'
-});
-$('.thumbnail').slick({
-slidesToShow: 3,
-slidesToScroll: 1,
-asNavFor: '.gallery',
-dots: false,
-centerMode: true,
-focusOnSelect: true
-});
-});
+$('.gallery-jumbo').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: false,
+   fade: true,
+   asNavFor: '.thumbnail'
+   });
+   $('.thumbnail').slick({
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   asNavFor: '.gallery-jumbo',
+   dots: false,
+   centerMode: true,
+   focusOnSelect: true
+   });
